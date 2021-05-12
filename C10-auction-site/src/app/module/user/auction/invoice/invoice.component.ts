848d2f4bc716d20368978e44dfc12b1186641f99
C1020G1_Tran_Huu_Kien <kienthd5h3@gmail.com>
2021-05-12 16:59:12 +0700

    1

--- C10-auction-site/src/app/module/user/auction/invoice/invoice.component.ts
+++ C10-auction-site/src/app/module/user/auction/invoice/invoice.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfMake from 'html-to-pdfmake';
+import * as html2pdf from 'html2pdf.js';
+import {Element} from '@angular/compiler';
 
 @Component({
   selector: 'app-invoice',
@@ -6,10 +14,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent implements OnInit {
+  // title = 'htmlToPdf';
+  // @ViewChild('pdfTable') pdfTable: ElementRef;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  public downloadAsPDF() {
+    const options = {
+      fileName: '123',
+      margin: [0, 0, 0, 0],
+      image: {type: 'jpeg'},
+      html2canvas: {
+        height: 2000
+      },
+      jsPDF: {
+        x: 0,
+        y: 0,
+        orientation: 'portrait'
+      },
+      format: 'a4',
+    };
+
+    const content: HTMLElement = document.getElementById('pdfTable');
+
+    html2pdf()
+      .from(content)
+      .set(options)
+      .save();
+  }
 }
