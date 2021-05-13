175d8844e7722c2b6ab54057cb4e378f70c3d8ea
huuhan2507@gmail.com <you@example.com>
2021-05-13 08:35:52 +0700

    update

--- C10-auction-site/src/app/module/user/home-page/list-product-home/list-product-home.component.ts
+++ C10-auction-site/src/app/module/user/home-page/list-product-home/list-product-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ListProductAuctionService} from './list-product-auction.service';
 
 @Component({
   selector: 'app-list-product-home',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductHomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private listProductAuctionService: ListProductAuctionService) {
   }
 
+  public listProduct: [];
+  public page = 1;
+  public activeOne = 'active';
+  public activeTwo;
+  public activeThree;
+  public activeFour;
+  public activeFive;
+  public category = 0;
+  ngOnInit(): void {
+    this.listProductAuctionService.showAllProductAuction(this.category).subscribe((data) => {
+      console.log(data);
+      this.listProduct = data;
+    });
+  }
+
+  showByAll() {
+    this.activeOne = 'active';
+    this.activeTwo = '';
+    this.activeThree = '';
+    this.activeFour = '';
+    this.activeFive = '';
+    this.category = 0;
+    this.ngOnInit();
+  }
+
+  showByHouseHold() {
+    this.activeOne = '';
+    this.activeTwo = 'active';
+    this.activeThree = '';
+    this.activeFour = '';
+    this.activeFive = '';
+    this.category = 2;
+    this.ngOnInit();
+  }
+  showByTechnology() {
+    this.activeOne = '';
+    this.activeTwo = '';
+    this.activeThree = 'active';
+    this.activeFour = '';
+    this.activeFive = '';
+    this.category = 1;
+    this.ngOnInit();
+  }
+
+  showByHot() {
+    this.activeOne = '';
+    this.activeTwo = '';
+    this.activeThree = '';
+    this.activeFour = 'active';
+    this.activeFive = '';
+    this.category = 4;
+    this.ngOnInit();
+  }
+
+  showByOther() {
+    this.activeOne = '';
+    this.activeTwo = '';
+    this.activeThree = '';
+    this.activeFour = '';
+    this.activeFive = 'active';
+    this.category = 3;
+    this.ngOnInit();
+  }
 }
