207724f5b6d50e6422d1edf27867e37bb7a91de5
phindepzai <phindeptroai@gmail.com>
2021-05-14 23:05:29 +0700

    update

--- C10-auction-site/src/app/module/user/header-footer/header-footer.module.ts
+++ C10-auction-site/src/app/module/user/header-footer/header-footer.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
@@ -8,7 +9,8 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [HeaderComponent, FooterComponent]
 })
