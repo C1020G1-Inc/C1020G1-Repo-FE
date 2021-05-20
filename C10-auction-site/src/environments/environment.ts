02dc1ae24532d9ca290c84c2fc805a4f90ee5298
duong1007 <maybiluyena@gmail.com>
2021-05-20 21:49:08 +0700

    update fuckpham

--- C10-auction-site/src/environments/environment.ts
+++ C10-auction-site/src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAeEkA0jE_MBTI6MPbyEH52woIlrmVNYBg",
-    authDomain: "c10auctionroom.firebaseapp.com",
-    databaseURL: "https://c10auctionroom-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "c10auctionroom",
-    storageBucket: "c10auctionroom.appspot.com",
-    messagingSenderId: "666650785329",
-    appId: "1:666650785329:web:67b70846f8184eea258710"
+    apiKey: 'AIzaSyCMPu7Fy3uJslFOEVlslwPi2JQCOK4qxV8',
+    authDomain: 'auction-589b3.firebaseapp.com',
+    databaseURL: 'https://auction-589b3-default-rtdb.firebaseio.com',
+    projectId: 'auction-589b3',
+    storageBucket: 'auction-589b3.appspot.com',
+    messagingSenderId: '109013631582',
+    appId: '1:109013631582:web:424c1e2ae3ebbf01deeacf',
+    measurementId: 'G-NE4HNXJGJX'
   }
 };
 
